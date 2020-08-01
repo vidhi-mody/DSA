9dc33dc689f8fe7f958d862022edddb447d1b275
vidhi-mody <vidhimody6@gmail.com>
2020-08-02 00:48:17 +0530

    add(GoldmanSachs): reverse-words-in-a-given-string.py

--- Interview_prep/Goldman_Sachs/reverse-words-in-a-given-string.py
+++ Interview_prep/Goldman_Sachs/reverse-words-in-a-given-string.py
@@ -1,13 +1,19 @@
 def reverse_words_in_a_given_string(string):
+    string = string.strip()
     n = len(string)
     words = []
     count = 0
     for i in range(n):
-        if string[i]==' ':
+        if string[i]==' ' and string[i+1]!=' ':
             words.append(string[count:i])
             count = i + 1
     words.append(string[count:])
-    return(words[::-1])     
+    words = words[::-1]
+    return " ".join(x.strip() for x in words)  
 
-answer = reverse_words_in_a_given_string("geeks quiz practice code")
+answer = reverse_words_in_a_given_string("     geeks    quiz    practice    code")
 print(answer)
+
+"""
+Output: code practice quiz geeks
+"""
