8e19bf768cc15a71120c23f2fe5892fa9dd5d95e
vidhi-mody <vidhimody6@gmail.com>
2021-03-28 02:07:51 +0530

    edit: make recursive-multiply.py optimal

--- Recursion/recursive-multiply.py
+++ Recursion/recursive-multiply.py
@@ -4,12 +4,15 @@
 def multiply(num1, num2):
     if(num2 == 0):
         return 0
-    else:
-        return(num1 + multiply(num1, num2-1))
+    # Add num1 one by one
+    if(num2 > 0 ):
+        return (num1 + multiply(num1, num2 - 1))
+ 
+    # The case where num2 is negative
+    if(num2 < 0 ):
+        return -multiply(num1, -num2)
     
 # Driver code
 num1 = 10
-num2 = 4
-max1 = max(num1, num2)
-min1 = min(num1,num2)
-print(multiply(max1, min1))
+num2 = 24
+print(multiply(num1, num2))
